ab74b3c64b69ac22427fcd434309145708e9de09
sunnyp48 <sunnyp48@ufl.edu>
2017-09-22 15:38:31 -0400

    changed port logic yo

--- server/config/app.js
+++ server/config/app.js
@@ -1,10 +1,11 @@
 var config = require('./config'), 
     mongoose = require('mongoose'),   
-    express = require('./express');
+    express = require('./express'),
+    port = process.env.PORT || 8080;
 
 module.exports.start = function() {
   var app = express.init();
-  app.listen(config.port, function() {
-    console.log('App listening on port', config.port);
+  app.listen(port, function() {
+    console.log('App listening on port', port);
   });
 };
