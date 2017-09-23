77e69178635bf2b6d659edc6cab3dfb3d7e363ff
Sunny Patel <patelsunny15@gmail.com>
2017-09-23 11:21:45 -0400

    Update app.js

--- server/config/app.js
+++ server/config/app.js
@@ -1,11 +1,11 @@
 var config = require('./config'), 
     mongoose = require('mongoose'),   
     express = require('./express'),
-    port = process.env.PORT || 8080;
+   
 
 module.exports.start = function() {
   var app = express.init();
-  app.listen(port, function() {
-    console.log('App listening on port', port);
+   app.listen(config.port, function() {
+    console.log('App listening on port', config.port);
   });
 };
